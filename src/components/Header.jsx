195c6fc0f1697d0705429109c1e36944993300cc
Killian <martykillian7@gmail.com>
2024-12-26 11:08:19 +0100

    Header cards responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import HeaderCards from './HeaderCards';
 const Header = () => {
   return (
     <>
-      <div className='bg-[#FAFAFA] relative top-[-41px] w-[1486px] h-[431px] border rounded-b-[24px] overflow-hidden'><HeaderCards/></div>
+      <div className='bg-[#FAFAFA] relative top-[-41px] w-[400px] sm:w-[670px] md:w-[850px] lg:w-[1100px] xl:w-[1486px] h-[211px] sm:h-[211px] md:h-[251px] lg:h-[331px] xl:h-[431px] border rounded-b-[24px] overflow-hidden'><HeaderCards/></div>
       
     </>
   );
